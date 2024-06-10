e2aa4ca04757e4ff869547fb1124d09c4fd37bda
Marco Villanueva <159150083+Marco-Villanueva-21@users.noreply.github.com>
2024-06-10 13:30:14 +0000

    cpuTurn is working but more testing is needed to find and clean up bugs

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 deck = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'RS', 'RR', 'R+2', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'BS', 'BR', 'B+2', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8', 'Y9', 'YS', 'YR', 'Y+2', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'GS', 'GR', 'G+2', 'WC', 'W+4']
 
 special = ['S', 'R', '+']
-CPUspecial = ['S', 'R', '+', 'W']
 colors = ['R', 'B', 'Y', 'G']
 
 print('Welcome to UNO!')
@@ -86,6 +85,9 @@ def drawnCard(add):
 #define CPU's ability to draw a card
 def drawnCardCPU(add):
   global playTurn
+  global turnNum
+  
+  playTurn = currentTurn()
   
   if checkValid(add):
     face = add
@@ -115,70 +117,56 @@ def countColor(cpuDeck):
 
   return redCount, blueCount, greenCount, yellowCount
 
-# def cpuTurn(cpuDeck):
-#   global turnNum
+def cpuTurn(cpuDeck):
+  global turnNum
+  global face
   
-#   #check each card in the cpu's deck
-#   valid = False
-#   for x in range (0, len(cpuDeck)):
-#     if checkValid(cpuDeck[0+x]):
-#       valid = True
+  playTurn = currentTurn()
+  validCards = []
+  valid = False
+  specialPlayed = False
+  
+  #check each card in the cpu's deck
+  for x in range (0, len(cpuDeck)):
+    if checkValid(cpuDeck[0+x]):
+      valid = True
+      validCards.append(cpuDeck[0+x])
 
-#   #if the user has a valid card
-#   if valid == True:
+  #if the cpu has a valid card, check for a special card
+  if valid:
+    for x in range (0, len(validCards)):
+      
+      #if a special card is found, play it
+      if validCards[0+x][1] in special or validCards[0+x][0] == 'W':
 
-#         #check if the card is in the user's deck
-#         if special in playerDeck:
+        specialPlayed = True
+        cpuWild(validCards[0+x], cpuDeck)
+        skip(validCards[0+x])
+        rev(validCards[0+x])
+        plus(validCards[0+x])
+        break
 
-#           if playCard[0] == 'W' or playCard[1] in special:
+    #check if a special card was not played and play a random card from the validCards list
+    if not specialPlayed:
+      playCard = random.choice(validCards)
+      face = playCard
+      cpuDeck.remove(playCard)
+      print(playTurn, 'played', playCard)
 
-#           #check if the played card is a wild card
-#           skip()
-#           rev()
-#           plus()
+      print('The current card on the playing pile is:', face)
 
-
-#               #if the user inputs a valid color, change the card to that color
-#               if color in colors:
-#                 face = color + 'C'
-#                 print('You changed the color to', color)
-#                 print('The current card on the playing pile is:', face)
-#                 chooseColor = True
-#                 played = True
-#               else: 
-#                 print('You picked an invalid color! Please try again!')
-#                 chooseColor = False
-
-#           #otherwise, check if the card is playable
-#           elif checkValid(playCard):
-#             face = playCard
-#             playerDeck.remove(playCard)
-#             print('You played', playCard)
-
-#             if playCard[1] in special or len(playCard) == 2:
-#               print('you played a special card!')
-#               skip()
-#               rev()
-#               plus()
-
-#             print('The current card on the playing pile is:', face)
-#             pDeck = ', '.join(str(x) for x in playerDeck)
-#             print('Your deck is:', str(pDeck))
-#             played = True
-
-#   #if cpu does not have a valid card, force to draw and play if possible, otherwise skip their turn
-#   if valid == False:
-#     add = random.choice(deck)
-#     cpuDeck.append(add)
-#     print(playTurn, 'drew a card!')
-#     drawnCard(add)
-#   turnNum = turnNum + (1*reverse)
+  #if cpu does not have a valid card, force to draw and play if possible, otherwise skip their turn
+  if not valid:
+    add = random.choice(deck)
+    cpuDeck.append(add)
+    print(playTurn, 'drew a card!')
+    drawnCard(add)
+  turnNum = turnNum + (1*reverse)
 
 #define the play again checker at the end of the game
 def playMore():
-  global playAgainCheck
-  global playAgain
-  global gameExit
+  global playAgainCheck, playAgain, gameExit
+  
   while not playAgainCheck:
     if playAgain == 'Y':
       print("Let's play again!")
@@ -190,7 +178,44 @@ def playMore():
       gameExit = False
     else:
       playAgain = False
-      
+
+#decide how a cpu plays a wild card
+def cpuWild(playCard, cpuDeck):
+  global turnNum
+  global face
+  countedColors = []
+  possible = []
+  
+  playTurn = currentTurn()
+  if playCard[0] == 'W':
+    
+    print(playTurn, 'has played a wild card!')
+    redCount, blueCount, greenCount, yellowCount = countColor(cpuDeck)
+    
+    countedColors.append(redCount)
+    countedColors.append(blueCount)
+    countedColors.append(greenCount)
+    countedColors.append(yellowCount)
+
+    mostColor = max(countedColors)
+
+    if mostColor == redCount:
+      possible.append('R')
+    if mostColor == blueCount:
+      possible.append('B')
+    if mostColor == greenCount:
+      possible.append('G')
+    if mostColor == yellowCount:
+      possible.append('Y')
+
+    chosenColor = random.choice(possible)
+
+    face = chosenColor + 'C'
+    cpuDeck.remove(playCard)
+    print(playTurn, 'changed the color to', chosenColor)
+    print('The current card on the playing pile is:', face)
+    return face
+    
 #define wild cards used by user (user can choose a color)
 def wild(playCard):
   chooseColor = False
@@ -220,30 +245,34 @@ def wild(playCard):
 #define skip cards (the next player's turn is skipped)
 def skip(playCard):
   global turnNum
-  global playTurn
+  playTurn = currentTurn()
+  
   if playCard[1] == 'S':
     turnNum = turnNum + (reverse*1)
-    playTurn = currentTurn()
+    print(playTurn, 'played', playCard, 'which is a skip card!')
     print(playTurn, 'had their turn skipped!')
 
 #define reverse cards (the order of the players reverse)
 def rev(playCard):
   global reverse
+  playTurn = currentTurn()
+  
   if playCard[1] == 'R':
     reverse = reverse*(-1)
+    print(playTurn, 'played', playCard, 'which is a reverse card!')
     print('The order of the players has been reversed!')
 
 #define plus cards (2 or 4 cards are forced to be added to the next player's deck and the next player's turn is skipped)
 def plus(playCard):
-  global turnNum
-  global reverse
-  global playTurn
-  global forceDraw
+  global turnNum, reverse, forceDraw
+
+  playTurn = currentTurn()
 
   if playCard[1] == '+':
     if playCard[2] == '2' or playCard[2] == '4':
       defended = True
       forceDraw = int(playCard[2])
+      print(playTurn, 'played', playCard, 'which is a plus card!')
   
       #loop back when a player has successfully defended a plus card
       while defended:
@@ -314,12 +343,14 @@ def plus(playCard):
 
               #force the user to draw and skip their turn if a plus card is not found
               else:
+                time.sleep(0.5)
                 print('You don\'t have a plus card! You must draw and skip your turn!')
                 for x in range(0, forceDraw):
                   cDeck = random.choice(deck)
                   playerDeck.append(cDeck)
                   defended = False
                   print('You drew', cDeck)
+                  time.sleep(0.25)
                 print('\nYou drew', forceDraw, 'cards in total! Yikes!')
                 
             #CPU1 is hit
@@ -329,6 +360,7 @@ def plus(playCard):
 
                 #if CPU1 fails to defend, force to draw and skip their turn
                 if cpu1Defended == False:
+                  time.sleep(0.5)
                   for x in range(0,forceDraw):
                     cDeck = random.choice(deck)
                     cpu1Deck.append(cDeck)
@@ -343,6 +375,7 @@ def plus(playCard):
 
                 #if CPU2 fails to defend, force to draw and skip their turn
                 if cpu2Defended == False:
+                  time.sleep(0.5)
                   for x in range(0,forceDraw):
                     cDeck = random.choice(deck)
                     cpu2Deck.append(cDeck)
@@ -357,6 +390,7 @@ def plus(playCard):
 
                 #if CPU3 fails to defend, force to draw and skip their turn
                 if cpu3Defended == False:
+                  time.sleep(0.5)
                   for x in range(0,forceDraw):
                     cDeck = random.choice(deck)
                     cpu3Deck.append(cDeck)
@@ -370,9 +404,7 @@ def plus(playCard):
 
 #define a function for CPU players to defend a plus card
 def cpuDefend(cpuDeck):
-  global forceDraw
-  global turnNum
-  global face
+  global forceDraw, turnNum, face
   
   playTurn = currentTurn()
 
@@ -429,7 +461,7 @@ while gameExit == True:
   playerDeck.append('R+2')
   playerDeck.append('B+2')
 
-  cpu1Deck.append('Y+2')
+  # cpu1Deck.append('WC')
   cpu2Deck.append('G+2')
   cpu3Deck.append('G+2')
   #turn the user's deck list into strings 
@@ -499,7 +531,7 @@ while gameExit == True:
 
                 #check if the second character is in the special list
                 if playCard[1] in special:
-                  print('You played a special card!')
+                  print('You played a special card!\n')
                   skip(playCard)
                   rev(playCard)
                   plus(playCard)
@@ -511,8 +543,7 @@ while gameExit == True:
       
           #if the user inputs draw, draw a card
           elif PlayDraw == 'draw':
-            # add = random.choice(deck)
-            add = 'W+4'
+            add = random.choice(deck)
             playerDeck.append(add)
             print('You drew a', add)
             drawnCard(add)
@@ -542,8 +573,9 @@ while gameExit == True:
     #enter CPU1's turn if currentTurn function returns CPU1
     elif currentTurn() == 'CPU1':
       print("\nIt is CPU1's turn!")
+      time.sleep(0.5)
       print("CPU1 has", len(cpu1Deck), "card(s)")
-      turnNum = turnNum + (1*reverse)
+      cpuTurn(cpu1Deck)
 
       #if CPU1 reaches zero cards, display the winner and ask to play again
       if len(cpu1Deck) == 0:
@@ -555,8 +587,9 @@ while gameExit == True:
     #enter CPU2's turn if currentTurn function returns CPU2
     elif currentTurn() == 'CPU2':
       print("\nIt is CPU2's turn!")
+      time.sleep(0.5)
       print("CPU2 has", len(cpu2Deck), "card(s)")
-      turnNum = turnNum + (1*reverse)
+      cpuTurn(cpu2Deck)
 
       #if CPU2 reaches zero cards, display the winner and ask to play again
       if len(cpu2Deck) == 0:
@@ -568,8 +601,9 @@ while gameExit == True:
     #enter CPU3's turn if currentTurn function returns CPU3
     elif currentTurn() == 'CPU3':
       print("\nIt is CPU3's turn!")
+      time.sleep(0.5)
       print("CPU3 has", len(cpu3Deck), "card(s)")
-      turnNum = turnNum + (1*reverse)
+      cpuTurn(cpu3Deck)
 
       #if the CPU3 reaches zero cards, display the winner and ask to play again
       if len(cpu3Deck) == 0:
